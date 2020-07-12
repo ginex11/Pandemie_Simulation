eae0f5b21994b61a46db986eb6b2ca1348517e9e
Konstantin <konstantin_miller@gmx.de>
2020-07-12 19:28:35 +0200

    Added first prototype of human motion

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,54 +4,41 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 import static sample.Const.*;
 
 public class Main extends Application {
-    int count = 0;
+    int count = 1;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("COVID19_Simulation");
         Pane pane = new Pane();
-        Random r = new Random();
-        Path path = new Path();
-
+        ArrayList<Mensch> people = new ArrayList<>();
+        Mensch m = new Mensch(true, pane);
+        people.add(m);
         while (count < 30) {
-
-            Mensch m = new Mensch(false, pane);
-            pane.getChildren().add(m.getCircle());
+            m = new Mensch(false, pane);
+            people.add(m);
             count++;
         }
-        Circle circle = new Circle(1 * r.nextInt(1280 - 1) + 1, 1 * r.nextInt(720 - 1) + 1, 4, Color.RED);
+        System.out.println(people.size());
 
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                Line traceLine = new Line();
-                traceLine.setStroke(Color.rgb(23, 0, 0, 0.5));
-                traceLine.setStrokeWidth(0.5);
-                traceLine.setStartX(circle.getCenterX());
-                traceLine.setStartY(circle.getCenterY());
-                circle.setCenterX(circle.getCenterX() + ARR[r.nextInt(2)]);
-                circle.setCenterY(circle.getCenterY() + ARR[r.nextInt(2)]);
-                traceLine.setEndX(circle.getCenterX());
-                traceLine.setEndY(circle.getCenterY());
-                pane.getChildren().add(traceLine);
+                people.get(0).move(ARR[RAND.nextInt(2)], ARR[RAND.nextInt(2)]);
             }
         };
-        pane.getChildren().addAll(circle, path);
 
         timer.start();
 
-        Scene scene = new Scene(pane, 1280, 720);
+        Scene scene = new Scene(pane, WIDTH, HEIGHT);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
