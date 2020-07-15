eaf065540af7d21202766a29f2b202341b43c671
Konstantin <konstantin_miller@gmx.de>
2020-07-15 18:14:48 +0200

    Update Const.java

--- src/sample/Const.java
+++ src/sample/Const.java
@@ -8,7 +8,7 @@ public class Const {
     public static final Random RAND = new Random();
     public static final double WIDTH = 1280.0;
     public static final double HEIGHT = 720.0;
-    public static final long FRAMES_PER_SEC = 30L;
+    public static final long FRAMES_PER_SEC = 60L;
     public static final long INTERVAL = 1000000000L / FRAMES_PER_SEC;
     public static final double CHANCE = 0.06;
 
