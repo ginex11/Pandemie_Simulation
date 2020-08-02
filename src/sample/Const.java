7f00a46c6afd75299b88fe76e37aa6fcfcfbd574
Konstantin <konstantin_miller@gmx.de>
2020-08-02 14:49:49 +0200

    update

--- src/sample/Const.java
+++ src/sample/Const.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class Const {
     public static final int RADIUS = 3;
-    public static final double SPEED = 1;
+    public static double SPEED = 1;
     public static final Random RAND = new Random();
-    public static final double WIDTH = 1200.0;
-    public static final double HEIGHT = 630.0;
+    public static final double WIDTH = 1051.0;
+    public static final double HEIGHT = 701.0;
     public static final long FRAMES_PER_SEC = 60L;
     public static final long INTERVAL = 1000000000L / FRAMES_PER_SEC;
     public static final double CHANCE = 0.13;
